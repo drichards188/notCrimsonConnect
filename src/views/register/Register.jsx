03a66b734a3c5dfc5726b7683f615dd6357e00c5
Conor Quinlan <conor.quinlan@du.edu>
2023-10-21 16:47:45 -0600

    created the Registration Form

---
+++ src/views/register/Register.jsx
@@ -0,0 +1,13 @@
+import "../../../src/components/style.css";
+import Header from '../../components/header';
+import RegistrationForm from "../../components/registrationForm";
+
+const Register = () => {
+    return (
+        <div className="App">
+            <Header/>
+            <RegistrationForm/>
+        </div>
+    )
+};
+export default Register;
