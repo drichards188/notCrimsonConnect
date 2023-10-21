c44addb379a2bf1ed0cc59a1871c4039ef59f92c
noah-fullerton <noahmfullerton@gmail.com>
2023-10-21 14:35:02 -0600

    firebase is setup with the project, hosting runs

---
+++ src/firebase_setup/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDtvJzUjNTCviwADQxGyheqtbxx7bkOhdY",
+  authDomain: "cldgnjhackathon.firebaseapp.com",
+  databaseURL: "https://cldgnjhackathon-default-rtdb.firebaseio.com",
+  projectId: "cldgnjhackathon",
+  storageBucket: "cldgnjhackathon.appspot.com",
+  messagingSenderId: "660997313876",
+  appId: "1:660997313876:web:2f76873c3b183a0982e4c1",
+  measurementId: "G-WHK4GQJKBF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+const auth = firebase.auth();
+export { auth, firestore};
