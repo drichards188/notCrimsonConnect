404e75b511c2ac3c3044aa5ae903f66f92109e3e
Conor Quinlan <conor.quinlan@du.edu>
2023-10-22 14:29:12 -0600

    Updated

---
+++ src/views/home/Home.jsx
@@ -0,0 +1,11 @@
+import "../../components/login_style.css";
+import Header from '../../components/header';
+
+const Register = () => {
+    return (
+        <div className="App">
+            <Header/>
+        </div>
+    )
+};
+export default Register;
