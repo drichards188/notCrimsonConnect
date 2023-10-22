13e6dca421d5638d15965229805861e00ffe697c
drichards188 <github@cxoprotect.com>
2023-10-22 06:19:46 -0600

    added an effect to get data and if only placeholder data then display fetching message

---
+++ src/components/lib/api.js
@@ -0,0 +1,44 @@
+export function getEvents() {
+    return [
+        {
+            "name": "TechFest 2023",
+            "start_date": "2023-11-01",
+            "end_date": "2023-11-03",
+            "location": "Engineering Building, Room 101",
+            "description": "A three-day event showcasing the latest technological innovations by students. Workshops, panels, and competitions included.",
+            "club_name": "Tech Club"
+        },
+        {
+            "name": "Art Showcase: Autumn Visions",
+            "start_date": "2023-11-15",
+            "end_date": "2023-11-20",
+            "location": "Campus Art Gallery",
+            "description": "An art exhibition featuring the works of budding artists, capturing the essence of autumn.",
+            "club_name": "Art Society"
+        },
+        {
+            "name": "Eco Day Fair",
+            "start_date": "2023-11-25",
+            "end_date": "2023-11-25",
+            "location": "Central Campus Lawn",
+            "description": "Join us for a day of eco-friendly workshops, stalls, and talks. Learn how you can make a difference for our planet.",
+            "club_name": "Eco Warriors"
+        },
+        {
+            "name": "Winter Wonderland Ball",
+            "start_date": "2023-12-10",
+            "end_date": "2023-12-10",
+            "location": "Campus Grand Hall",
+            "description": "A magical night of dance, music, and winter-themed festivities. Formal attire required.",
+            "club_name": "Dance Ensemble"
+        },
+        {
+            "name": "Entrepreneurship Pitch Night",
+            "start_date": "2023-12-18",
+            "end_date": "2023-12-18",
+            "location": "Business Auditorium, Room 201",
+            "description": "Witness the next generation of entrepreneurs as they pitch their innovative business ideas to a panel of industry experts.",
+            "club_name": "Entrepreneurship Club"
+        }
+    ]
+}
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,55 +1,29 @@
 import logo from "../../logo.svg";
 import "../../App.css";
-import Login from "../../components/login/login";
 import {Box, Card, CardContent, Grid, Paper} from "@mui/material";
 import EventThumb from "../../components/event/EventThumb";
 import NavigateButton from "../../components/lib/NavigateButton";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {getEvents} from "../../components/lib/api";
 
 const Home = () => {
-    const [eventData, setEventData] = useState([
-            {
-                "name": "TechFest 2023",
+    const [eventData, setEventData] = useState([{
+                "name": "Name Placeholder",
                 "start_date": "2023-11-01",
                 "end_date": "2023-11-03",
-                "location": "Engineering Building, Room 101",
-                "description": "A three-day event showcasing the latest technological innovations by students. Workshops, panels, and competitions included.",
-                "club_name": "Tech Club"
-            },
-            {
-                "name": "Art Showcase: Autumn Visions",
-                "start_date": "2023-11-15",
-                "end_date": "2023-11-20",
-                "location": "Campus Art Gallery",
-                "description": "An art exhibition featuring the works of budding artists, capturing the essence of autumn.",
-                "club_name": "Art Society"
-            },
-            {
-                "name": "Eco Day Fair",
-                "start_date": "2023-11-25",
-                "end_date": "2023-11-25",
-                "location": "Central Campus Lawn",
-                "description": "Join us for a day of eco-friendly workshops, stalls, and talks. Learn how you can make a difference for our planet.",
-                "club_name": "Eco Warriors"
-            },
-            {
-                "name": "Winter Wonderland Ball",
-                "start_date": "2023-12-10",
-                "end_date": "2023-12-10",
-                "location": "Campus Grand Hall",
-                "description": "A magical night of dance, music, and winter-themed festivities. Formal attire required.",
-                "club_name": "Dance Ensemble"
-            },
-            {
-                "name": "Entrepreneurship Pitch Night",
-                "start_date": "2023-12-18",
-                "end_date": "2023-12-18",
-                "location": "Business Auditorium, Room 201",
-                "description": "Witness the next generation of entrepreneurs as they pitch their innovative business ideas to a panel of industry experts.",
-                "club_name": "Entrepreneurship Club"
-            }
-        ]
-    );
+                "location": "Location Placeholder",
+                "description": "Description Placeholder",
+                "club_name": "Club Name Placeholder"
+            }]);
+
+    useEffect(() => {
+        if (eventData === undefined || eventData.length < 2) {
+            const fetchedEventData = getEvents();
+            setEventData(fetchedEventData);
+        }
+    }, [eventData]);
+
+
 
     return (
         <div className="App">
@@ -58,7 +32,7 @@ const Home = () => {
 
                         <Grid item sm={4} style={{}}>
                             <Paper elevation={6}>
-                                NotCrimsonConnect
+                                MSU Denver NotCrimsonConnect
                             </Paper>
                         </Grid>
 
@@ -69,6 +43,9 @@ const Home = () => {
                             </Paper>
                         </Grid>
                     </Grid>
+
+                {eventData.length < 2 &&
+                <h1>... fetching event data</h1>}
 
                     <Grid container spacing={2}
                           direction="column"
